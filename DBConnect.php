f328337505a97bcbd397fcdf4f36550fa2a739db
NND <nguyenduc3320@gmail.com>
2021-01-08 15:13:52 +0700

    add class Dataprovider

--- DBConnect.php
+++ DBConnect.php
@@ -46,3 +46,30 @@ function LoadData($sql)
     #CloseConnection();
     return $result;
 }
+
+class DataProvider 
+{
+	public static function ExecuteQuery($sql)
+	{
+		$connection = mysqli_connect('localhost','root','') or
+			die ("couldn't connect to localhost");
+
+		
+		mysqli_select_db($connection, 'Ebook');
+				
+		mysqli_query($connection, "set names 'utf8'");
+		
+		$result = mysqli_query($connection, $sql);
+		
+		mysqli_close($connection);
+		
+		return $result;
+	}
+	
+	public static function ChangeURL($path)
+	{
+		echo '<script type = "text/javascript">';
+		echo 'location = "'.$path.'";';
+		echo '</script>';
+	}
+}
